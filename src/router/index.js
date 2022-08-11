d7be67b4f5107587ae4f1ad19f9d54f28f5bdeb7
valentin ranshakov <mvalenso@gmail.com>
2022-08-11 14:30:00 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import OfferView from '../views/OfferView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/offer',
       name: 'offer',
-      component: () => import('../views/OfferView.vue')
+      component: OfferView
     }
   ]
 })
