888ef8b991bbe56eb91a21f246182dbd3acf4fc9
valentin ranshakov <mvalenso@gmail.com>
2022-08-11 14:53:03 +0200

    set width

--- src/components/SubmitOffer.vue
+++ src/components/SubmitOffer.vue
@@ -61,7 +61,7 @@ export default {
 </script>
 
 <template>
-  <form class="w-full max-w-sm">
+  <form class="w-full max-w-lg">
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
       <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="product-title">
